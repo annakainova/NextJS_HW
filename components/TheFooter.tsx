b1e389939397e6bd708c162da87a3eac8159f00c
Anna Kainova <annakainova88@gmail.com>
2023-08-07 16:24:24 +0200

    1 part

---
+++ components/TheFooter.tsx
@@ -0,0 +1,5 @@
+const TheFooter = () => {
+  return <footer>Created by &copy;AnnaK</footer>;
+};
+
+export { TheFooter };
---
+++ components/TheHeader.tsx
@@ -0,0 +1,16 @@
+import { Navigation } from "./Navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
+const TheHeader = () => {
+  return (
+    <header>
+      <Navigation navLinks={navItems} />
+    </header>
+  );
+};
+
+export { TheHeader };
